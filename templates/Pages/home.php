cb975c707dee107749b18dc02094172761b7fa8a
andres-casta <afcastanedar@outlook.com>
2022-03-07 12:06:26 -0500

    Se elimina depuracion

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -41,9 +41,9 @@ $checkConnection = function (string $name) {
     return compact('connected', 'error');
 };
 
-if (!Configure::read('debug')==0) :
+if (Configure::read('debug')==0) :
     throw new NotFoundException(
-        'Please replace templates/Pages/home.php with your own version or re-enable debug mode.'
+       
     );
 endif;
 
